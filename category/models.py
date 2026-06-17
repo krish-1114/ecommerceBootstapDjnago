125fe644c5ce66021fa9ba8e7640e0ff9639b1a9
Krish Suthar <skrish6353@gmail.com>
2026-06-17 18:05:40 +0530

    make customer usermodel add accounts make models for superuser

--- category/models.py
+++ category/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'Category'
-        verbose_name_plural = 'Categories   '
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.catergory_name   
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'category'
+    'category',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
+AUTH_USER_MODEL = 'accounts.account'
+
 
 
 # Database
