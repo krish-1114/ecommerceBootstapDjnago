c4242eb9804f5bebe6426dd514ec6947a5590f99
Krish Suthar <skrish6353@gmail.com>
2026-06-17 20:39:53 +0530

    Make slug auto type by populated_filed make changes in cat/adm and models

--- category/models.py
+++ category/models.py
@@ -1,33 +1,14 @@
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
-    catergory_name = models.CharField(max_length=50,unique=True)
-    slud = models.CharField(max_length=100,unique=True)
-    description = models.TextField(max_length=255,blank=True)
-    cat_image = models.ImageField(upload_to='photos/categories/',blank=True)
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories/', blank=True)
 
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.catergory_name   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return self.category_name
