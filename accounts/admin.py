c9a74bb26a517356f7aede92173e0b38f9710bb5
Krish Suthar <skrish6353@gmail.com>
2026-06-17 18:20:03 +0530

    Improve ment in admin that password not visible

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import account
-# Register your models here.
 
-admin.site.register(account)
+
+class AccountAdmin(UserAdmin):
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'username',
+        'last_login',
+        'is_active',
+        'date_joined'
+    )
+
+    ordering = ('email',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(account, AccountAdmin)
