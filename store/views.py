6b63d981a5fde861482f30d57440c2b846d12f52
Krish Suthar <skrish6353@gmail.com>
2026-06-19 22:04:03 +0530

    Product By catergory improve view and urls in store

--- store/views.py
+++ store/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import Category
 
-# Create your views here.
-def store(req):
-    products = Product.objects.all().filter(is_Available = True)
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(Category=categories, is_Available=True)
+    else:
+        products = Product.objects.filter(is_Available=True)
+
     product_count = products.count()
-    
+
     context = {
-        'products':products,
-        'product_count':product_count
+        'products': products,
+        'product_count': product_count,
     }
-    
-    return render(req,'store/store.html',context)
+
+    return render(request, 'store/store.html', context)
