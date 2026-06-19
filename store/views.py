c4b2fb3d5de5f864eb3bb8f86c7d26147c2a2301
Krish Suthar <skrish6353@gmail.com>
2026-06-19 18:00:29 +0530

    Store page db added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def store(req):
-    return render(req,'store/store.html')
+    products = Product.objects.all().filter(is_Available = True)
+    product_count = products.count()
+    
+    context = {
+        'products':products,
+        'product_count':product_count
+    }
+    
+    return render(req,'store/store.html',context)
